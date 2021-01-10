0d1d81e741caf9af8d042575729fad8214f8d4f5
kotaoue <kotaoue@gmail.com>
2021-01-10 21:54:56 +0900

    update STLLoader

--- docs/jsm/loaders/STLLoader.d.ts
+++ docs/jsm/loaders/STLLoader.d.ts
@@ -7,10 +7,10 @@ import {
 
 export class STLLoader extends Loader {
 
-	constructor( manager?: LoadingManager );
+	constructor(manager?: LoadingManager);
 
-	load( url: string, onLoad: ( geometry: BufferGeometry ) => void, onProgress?: ( event: ProgressEvent ) => void, onError?: ( event: ErrorEvent ) => void ) : void;
-	loadAsync( url: string, onProgress?: ( event: ProgressEvent ) => void ): Promise<BufferGeometry>;
-	parse( data: ArrayBuffer | string ) : BufferGeometry;
+	load(url: string, onLoad: (geometry: BufferGeometry) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void): void;
+	loadAsync(url: string, onProgress?: (event: ProgressEvent) => void): Promise<BufferGeometry>;
+	parse(data: ArrayBuffer | string): BufferGeometry;
 
 }
